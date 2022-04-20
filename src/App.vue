3928c25a07d738feb0a6009e5dc0afa545fe31e8
Arnon Rodrigues <arnonrdp@gmail.com>
2022-04-20 13:51:00 -0300

    Criada Molécula de Serviço

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,19 @@
     <NavBar />
     <SearchForm />
     <SocialNetworks />
+    <Service
+      title="Front-End"
+      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nostrum corrupti iste quam rem et fugit accusantium dolor atque laborum dolorem est, officia quisquam praesentium vel inventore, saepe sequi vero!"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { NavBar, SearchForm, SocialNetworks } from '@/components/molecules'
+import { NavBar, SearchForm, SocialNetworks, Service } from '@/components/molecules'
 
 export default defineComponent({
-  components: { NavBar, SearchForm, SocialNetworks }
+  components: { NavBar, SearchForm, SocialNetworks, Service }
 })
 </script>
 
