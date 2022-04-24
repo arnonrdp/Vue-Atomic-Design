8ac13be8814a90a3148c490c20c193ea78901525
arnonrdp <arnonrdp@gmail.com>
2022-04-24 13:51:00 -0300

    Criado Bóson Container

---
+++ src/components/bosons/Container.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  max-width: 1150px;
+  width: 85%;
+}
+</style>
